0880f703d7f47ac0910b17c5357901100fd23f5e
Akash Kumar Singh <44548740+akashakara@users.noreply.github.com>
2020-03-03 02:08:21 +0530

    face_detect.py

---
+++ face_detect.py
@@ -0,0 +1,32 @@
+import numpy as np 
+import cv2
+
+face_cascade = cv2.CascadeClassifier('data/haarcascade_frontalface_alt2.xml')
+#face cascade is given inside data folder
+cap=cv2.VideoCapture(0)
+while (True):
+
+    ret,frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    for (x,y,w,h) in faces:
+        print(x,y,w,h)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h,x:x+w]
+        img_item = "my_image.png"
+        cv2.imwrite(img_item,roi_gray)
+
+        color = (255,0,0)#BGR 0-255 means only blue , (0,255,0) means only green in that order
+        stroke = 2
+        end_cord_x = x + w
+        end_cord_y = y + h
+        cv2.rectangle(frame,(x,y),(end_cord_x,end_cord_y),color, stroke)
+
+
+
+    cv2.imshow("frame",frame)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+#code by code freaks @ akash
+cap.release()
+cv2.destroyAllWindows()
